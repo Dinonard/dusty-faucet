7b47882a74758240e01ca737728b27fb43199088
Kentaro Vadney <kentarovadney@Kentaros-MacBook-Air.local>
2021-06-11 12:01:16 -0700

    attempted swaping import with require

--- app/src/helpers/sendTokens.ts
+++ app/src/helpers/sendTokens.ts
@@ -1,12 +1,19 @@
 import { WsProvider, Keyring, ApiPromise } from '@polkadot/api';
-import { cryptoWaitReady, checkAddress } from '@polkadot/util-crypto';
-import { formatBalance } from '@polkadot/util';
-import { Balance } from '@polkadot/types/interfaces';
-import { createType, GenericAccountId } from '@polkadot/types';
+// const WsProvider = require('@polkadot/api').WsProvider;
+// const Keyring = require('@polkadot/api').Keyring;
+// const ApiPromise = require('@polkadot/api').ApiPromise;
+
+// import { cryptoWaitReady, checkAddress } from '@polkadot/util-crypto'; //TODO check input address
+// import { formatBalance } from '@polkadot/util'; //TODO check input address
+// import { Balance } from '@polkadot/types/interfaces'; //TODO check input address
+// import { createType, GenericAccountId } from '@polkadot/types'; //TODO check input address
 import type { RegistryTypes } from '@polkadot/types/types';
 const typeDefs = require('@plasm/types');
+
 import { ContractPromise } from '@polkadot/api-contract';
-import { Message } from 'discord.js';
+// const ContractPromise = require('@polkadot/api-contract').ContractPromise;
+
+const Message = require('discord.js').Message;
 
 const MNEMONIC = process.env.MNEMONIC!;
 const value = 0;
@@ -16,7 +23,7 @@ const AMOUNT = process.env.AMOUNT;
 const ADDRESS: string = process.env.ADDRESS?.toString()!;
 const ABI = require('./metadata.json');
 
-export const sendTokens = async (args: Array<string>, message: Message) => {
+export const sendTokens = async (args: Array<string>, message: typeof Message) => {
     // may need to be GeneralAccountID type
     const to: string = args[0]!;
     const keyring = new Keyring({ type: 'sr25519' });
