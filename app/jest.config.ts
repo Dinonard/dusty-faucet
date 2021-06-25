577e5b658152a1c7a9a604d82a43633d1d383839
Hoon Kim <mail@hoonkim.me>
2021-06-25 15:55:01 +0900

    add esm flag to node

--- app/jest.config.ts
+++ app/jest.config.ts
@@ -6,12 +6,13 @@ const config: Config.InitialOptions = {
     testEnvironment: 'node',
     verbose: true,
     preset: 'ts-jest/presets/default-esm',
+    extensionsToTreatAsEsm: ['.ts'],
     globals: {
         'ts-jest': {
             useESM: true,
         },
     },
-    roots: ['<rootDir>/.'],
+    roots: ['<rootDir>/tests/'],
     testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
     transform: {
         '^.+\\.(ts|tsx)$': 'ts-jest',
